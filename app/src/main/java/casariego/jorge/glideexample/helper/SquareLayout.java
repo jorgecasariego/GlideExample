cae2a9bd688509f2a2643798baff05e7b8a97f3f
Jorge Casariego <jorgekasa@gmail.com>
2016-09-28 18:05:04 -0400

    Glide implemented with Volley and Detail showing with a Full Dialog

---
+++ app/src/main/java/casariego/jorge/glideexample/helper/SquareLayout.java
@@ -0,0 +1,36 @@
+package casariego.jorge.glideexample.helper;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by jorgecasariego on 28/9/16.
+ */
+
+public class SquareLayout extends RelativeLayout {
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
